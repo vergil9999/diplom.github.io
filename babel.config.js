f34e27fb0102aba0f63a5c6a936bcbad709f5fd6
Aleksandr Smirnov <chric2015@icloud.com>
2020-02-09 20:02:44 +0300

    final

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+const presets = [
+  [
+  "@babel/env",
+  {
+    targets: { // версии браузеров которые нужно поддерживать
+      edge: "15",
+      firefox: "60",
+      chrome: "64",
+      chromeAndroid: "64",
+      safari: "11.1"
+    },
+    useBuiltIns: "usage", // эта настройка babel-polyfill, если стоит значение usage, то будут подставлятся полифилы для версий браузеров которые указали выше.
+    corejs: "3.0.0", // явно проставить версию corejs
+    "targets": { // указать цели, для полифилов
+      "esmodules": true, // es модули
+      "ie": "11" // Internet Explorer 11
+    }
+  },
+  ],
+];
+
+module.exports = { presets };
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./pages/index.css";
+
+function showCards() {
+    document.querySelector('.preloader').setAttribute('style', 'display:none');
+  
+  }
+  
